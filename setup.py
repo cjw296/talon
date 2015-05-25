f64c3f209bc0bdb70ae49061121028e3da4bd2c3
chris <chris@simplistix.co.uk>
2015-05-25 20:27:56 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='talon',
+    author='Chris Withers',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    )
